9f681801ad607f7cd2e4a22effe1f877c239a721
Young <kimyoh123@gmail.com>
2025-07-06 20:03:56 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+let buttons = document.querySelectorAll(".drum");
+
+for (i = 0; i < buttons.length; i++) {
+
+    buttons[i].addEventListener("click", function () {
+        let buttonInner = this.innerHTML;
+        makeSound(buttonInner);
+
+    });
+}
+document.addEventListener("keydown", function (e) {
+    makeSound(e.key);
+})
+
+function makeSound(key) {
+    let file = 'sounds/';
+    switch (key) {
+        case "w":
+            file += 'tom-1.mp3';
+            break;
+        case "a":
+            file += 'tom-2.mp3';
+            break;
+        case "s":
+            file += 'tom-3.mp3';
+            break;
+        case "d":
+            file += 'tom-4.mp3';
+            break;
+        case "j":
+            file += 'snare.mp3';
+            break;
+        case "k":
+            file += 'crash.mp3';
+            break;
+        case "l":
+            file += 'kick-bass.mp3';
+            break;
+        default:
+            console.log(buttonInner);
+    }
+    let audio = new Audio(file);
+    audio.play();
+}
